8417bb4b283612393e81d9b719026a48dfa5dfc3
Mingde Yin <54449457+itchono@users.noreply.github.com>
2022-06-11 15:52:13 -0400

    Basic physics models

--- new_python/propagate_orbit.py
+++ new_python/propagate_orbit.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def propagate_orbit(t_span: np.array, y0: np.array,
                     force_model: Callable, solver: Callable,
-                    h0: float = 50, etol: float = 1e-6) -> PropagatorSolution:
+                    etol: float = 1e-6, h0: float = 50) -> PropagatorSolution:
     '''
     Propagates orbit using a given solver.
 
@@ -25,36 +25,36 @@ def propagate_orbit(t_span: np.array, y0: np.array,
         function for which y' = f(t, y) (ODE)
     solver: Callable
         solver which performs integration
-    h0: float
-        initial step size, in seconds
     etol: float
         error tolerance for position (m)
+    h0: float
+        initial step size, in seconds
     '''
 
-    solution = solver(force_model, y0, t_span, h0, etol)
+    solution = solver(force_model, y0, t_span, etol, h0)
 
     return solution
 
 if __name__ == '__main__':
     # Test code
-    t_span = np.array([0, 0.1])  # propagate for one day
-    y0 = np.array([6928e3, 0, 0, 0, 0.1e3, 7.5e3])  # initial state
+    t_span = np.array([0, 100])  # propagate for one day
+    y0 = np.array([-4749231.102296294, -4975106.82469687, 0.0, -719.6538503589323, 686.980716081442, 7561.282735263496])  # initial state
 
     # Propagate using RK1012
-    solution = propagate_orbit(t_span, y0, J2_gravity, RK1012_solve, etol=1e-12)
+    solution = propagate_orbit(t_span, y0, J2_gravity, RK1012_solve, etol=100)
     print(solution)
 
     # Plot results
-    plt.plot(solution.seconds_elapsed, solution.y[0, :], label='x')
-    plt.plot(solution.seconds_elapsed, solution.y[1, :], label='y')
-    plt.plot(solution.seconds_elapsed, solution.y[2, :], label='z')
-    plt.legend()
-    plt.show()
+    # plt.plot(solution.seconds_elapsed, solution.y[0, :], label='x')
+    # plt.plot(solution.seconds_elapsed, solution.y[1, :], label='y')
+    # plt.plot(solution.seconds_elapsed, solution.y[2, :], label='z')
+    # plt.legend()
+    # plt.show()
 
     # 3D plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot(solution.y[0, :], solution.y[1, :], solution.y[2, :], "o-")
+    ax.plot(solution.y[0, :], solution.y[1, :], solution.y[2, :])
     ax.set_xlabel('x (m)')
     ax.set_ylabel('y (m)')
     ax.set_zlabel('z (m)')
--- new_python/simplified_physics.py
+++ new_python/simplified_physics.py
@@ -3,6 +3,18 @@ import numpy as np
 
 GM = 3.986004418e14  # m^3/s^
 J2 = 1.082626925638742e-3  # m^2/s^2
+RE = 6378.137e3  # m
+
+def simple_gravity(t: float, y: np.array) -> np.array:
+    '''
+    Calculates gravitational forces on spacecraft
+    using GM/r^2
+    '''
+    position = y[:3]
+
+    r = np.linalg.norm(position)
+    a_g = -GM/r**3 * position  # acceleration due to GM/r^2
+    return np.concatenate((y[3:], a_g))
 
 def J2_gravity(t: float, y: np.array) -> np.array:
     '''
@@ -12,10 +24,11 @@ def J2_gravity(t: float, y: np.array) -> np.array:
     position = y[:3]
 
     r = np.linalg.norm(position)
-    r_3 = r**3
+    factor = (3.0/2.0) * GM * RE**2 * J2 / r**5
+    z = position[2]
 
-    a_g = -GM/r_3 * position  # acceleration due to GM/r^2
-    a_J2 = -J2 * r_3 * np.cross(position, position)  # acceleration due to J2
+    a_g = -GM/r**3 * position  # acceleration due to GM/r^2
+    a_J2 = factor * position * np.array([5*z**2/r**2 - 1, 5*z**2/r**2 - 1, 5*z**2/r**2 - 3])
 
     return np.concatenate((y[3:], a_J2 + a_g))
 
