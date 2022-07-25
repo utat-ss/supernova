6086ee7714f80dd3a8cd5c40e2fab5c009660e8b
Mingde Yin <mingdeyin@gmail.com>
2022-07-24 22:52:40 -0400

    Demo

--- python_code/plotter.py
+++ python_code/plotter.py
@@ -2,6 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def set_axes_equal(ax: plt.Axes):
+    """Set 3D plot axes to equal scale.
+
+    Make axes of 3D plot have equal scale so that spheres appear as
+    spheres and cubes as cubes.  Required since `ax.axis('equal')`
+    and `ax.set_aspect('equal')` don't work on 3D.
+    """
+    limits = np.array([
+        ax.get_xlim3d(),
+        ax.get_ylim3d(),
+        ax.get_zlim3d(),
+    ])
+    origin = np.mean(limits, axis=1)
+    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+    _set_axes_radius(ax, origin, radius)
+
+
+def _set_axes_radius(ax, origin, radius):
+    x, y, z = origin
+    ax.set_xlim3d([x - radius, x + radius])
+    ax.set_ylim3d([y - radius, y + radius])
+    ax.set_zlim3d([z - radius, z + radius])
+
+
 def plot_trajectory(*filenames: str):
     fig = plt.figure(figsize=(18, 7), tight_layout=True)
 
@@ -43,7 +67,7 @@ def plot_from_array(t, arr):
     axes = []
 
     for i in range(3):
-        axes.append(fig.add_subplot(231 + i))
+        axes.append(fig.add_subplot(131 + i))
         axes[i].add_patch(earth[i])
         axes[i].set_aspect("equal")
         axes[i].grid()
@@ -51,25 +75,52 @@ def plot_from_array(t, arr):
     x = arr[:, 0]
     y = arr[:, 1]
     z = arr[:, 2]
-    axes[0].plot(x, y)
+    axes[0].plot(x, y, color="green")
     axes[0].set_ylabel("y position (m)")
     axes[0].set_xlabel("x position (m)")
-    axes[1].plot(y, z)
+    axes[1].plot(y, z, color="green")
     axes[1].set_ylabel("z position (m)")
     axes[1].set_xlabel("y position (m)")
-    axes[2].plot(x, z)
+    axes[2].plot(x, z, color="green")
     axes[2].set_ylabel("z position (m)")
     axes[2].set_xlabel("x position (m)")
     axes[0].set_title("xy")
     axes[1].set_title("yz")
     axes[2].set_title("xz")
 
-    axes.append(fig.add_subplot(212))
-    # Plot altitude above each
-    altitude = np.sqrt(x**2 + y**2 + z**2) - 6.378e6
-    axes[3].plot(t/86400, altitude)
-    axes[3].set_ylabel("Altitude (m)")
-    axes[3].set_xlabel("Mission Time (d)")
+    plt.suptitle("Spacecraft Position, Inertial Coordinates")
+
+    plt.show()
+
+
+def plot_3d_from_array(t, arr):
+    fig = plt.figure(figsize=(6, 6), tight_layout=True)
+    ax = plt.axes(projection='3d')
+
+    x = arr[:, 0]
+    y = arr[:, 1]
+    z = arr[:, 2]
+
+    ax.plot(x, y, z, color="green")
+
+    # Set the labels
+    ax.set_xlabel('x (m)')
+    ax.set_ylabel('y (m)')
+    ax.set_zlabel('z (m)')
+
+    u = np.linspace(0, 2*np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x = np.outer(np.cos(u), np.sin(v))  # np.outer() -> outer vector product
+    y = np.outer(np.sin(u), np.sin(v))
+    z = np.outer(np.ones(np.size(u)), np.cos(v))
+    ax.plot_surface(x*6378e3, y*6378e3, z*6378e3)
+
+    # Set the title
+    ax.set_title("Spacecraft Position, Inertial Coordinates")
+
+    ax.set_box_aspect([1, 1, 1])  # IMPORTANT - this is the new, key line
+    # ax.set_proj_type('ortho') # OPTIONAL - default is perspective (shown in image above)
+    set_axes_equal(ax)  # IMPORTANT - this is also required
 
     plt.show()
 
--- python_code/supernova.py
+++ python_code/supernova.py
@@ -1,6 +1,8 @@
 from ctypes import CDLL, POINTER, c_double, c_int, c_char_p, Structure
 import numpy as np
-from plotter import plot_from_array
+from plotter import plot_from_array, plot_3d_from_array
+from celest.satellite import Time, Coordinate, Satellite
+from celest.encounter import GroundPosition, windows
 
 '''
 Code Responsible for Loading C Library
@@ -47,10 +49,12 @@ if __name__ == "__main__":
     tSpan = (c_double * len(py_tSpan))(*py_tSpan)
 
     state: POINTER(c_double) = state(6378e3+270e3, 0, 97 * np.pi/180, 0, 0, 0)
-    solution: POINTER(AdaptiveSolution) = orbit("RK810".encode("utf-8"), "advanced".encode("utf-8"), tSpan, state, 1e-6)
 
-    # py_y0 = [-4749231.102296294, -4975106.82469687, 0.0, -719.6538503589323, 686.980716081442, 7561.282735263496]
-    # y0 = (c_double * len(py_y0))(*py_y0)
+    py_y0 = [-4749231.102296294, -4975106.82469687, 0.0, -
+             719.6538503589323, 686.980716081442, 7561.282735263496]
+    y0 = (c_double * len(py_y0))(*py_y0)
+    solution: POINTER(AdaptiveSolution) = orbit("RK810".encode(
+        "utf-8"), "simplified".encode("utf-8"), tSpan, y0, 1e-6)
 
     n = solution[0].n  # Dereference pointer
     print(f"Steps taken: {n}")
@@ -58,3 +62,31 @@ if __name__ == "__main__":
     y = np.ctypeslib.as_array(solution[0].y, shape=(n, 6))
     initial_state = np.ctypeslib.as_array(state, shape=(6,))
     plot_from_array(t, y)
+    plot_3d_from_array(t, y)
+
+    # Celest stuff
+    julian = Time(t/86400)
+    position = Coordinate(y[:, :3], "gcrs", t/86400)
+
+    toronto = GroundPosition(latitude=43.6532, longitude=-79.3832)
+    southern_manitoba = GroundPosition(latitude=53.7167, longitude=-98.8167)
+
+    # Get ITRS data.
+    itrs_x, itrs_y, itrs_z = position.itrs()
+
+    # plot_from_array(t, np.vstack((itrs_x, itrs_y, itrs_z)).T)
+
+    alt, az = position.horizontal(southern_manitoba)
+
+    satellite = Satellite(position=y[:, :3], frame='gcrs', julian=t/86400,
+                          offset=2414900)
+
+    satellite.itrs()
+
+    # Generate ground location windows.
+    IMG_windows = windows.generate(
+        satellite=satellite, location=southern_manitoba, enc="image", ang=30, lighting=1)
+    GL_windows = windows.generate(
+        satellite=satellite, location=toronto, enc="data_link", ang=10, lighting=0)
+
+    print("hello")
